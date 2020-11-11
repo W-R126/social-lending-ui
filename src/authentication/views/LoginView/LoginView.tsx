78b15f1950998d4b5d78c11b256f6bfb25f21e1f
rz-roy <rz.royzhang@gmail.com>
2020-11-11 14:00:43 +0100

    remove debug message from view

--- src/authentication/views/LoginView/LoginView.tsx
+++ src/authentication/views/LoginView/LoginView.tsx
@@ -3,16 +3,10 @@ import {LoginForm} from '../../components/LoginForm';
 import {Box, Flex, Text} from '@chakra-ui/core';
 import {Routes} from '../../../routing/routes';
 import {AppLink} from '../../../routing/components/AppLink';
-import {useAuth} from '../../context/AuthProvider';
 
 export const LoginView: React.FC = () => {
-    const {isAuthenticated} = useAuth();
-
     return (
         <Flex width={'full'} p={4} align={'center'} justifyContent={'center'} direction={'column'}>
-            {/*DEBUG, REMOVE BEFORE MERGING*/}
-            <div>{isAuthenticated ? 'user is authenticated' : 'user is not authenticated'}</div>
-            {/*DEBUG, REMOVE BEFORE MERGING*/}
             <LoginForm />
             <Box width={'full'} maxWidth={500} my={6} p={2} borderWidth={1} borderRadius={8} boxShadow={'lg'}>
                 <Text display={'inline'}>Are you new here? </Text>
