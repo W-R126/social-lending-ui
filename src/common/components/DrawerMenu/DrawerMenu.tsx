08652de40c409bc84225301e80565863486c107e
rz-roy <rz.royzhang@gmail.com>
2020-11-12 22:25:42 +0100

    add logo to toolbar

--- src/common/components/DrawerMenu/DrawerMenu.tsx
+++ src/common/components/DrawerMenu/DrawerMenu.tsx
@@ -8,7 +8,9 @@ import {
     DrawerHeader,
     DrawerOverlay,
     Flex,
+    Heading,
     IconButton,
+    Image,
     Stack,
     Text,
     useDisclosure,
@@ -22,12 +24,16 @@ export const DrawerMenu: React.FC = () => {
     const {isAuthenticated} = useAuth();
     return (
         <>
-            <Flex as={'nav'} align={'center'} justify={'space-between'} p={3} shadow={'sm'}>
-                <Text>Otter - The Social Lending App</Text>
+            <Flex as={'nav'} align={'center'} justify={'space-between'} p={2} shadow={'sm'}>
+                <span>
+                    <Image src={'logo/logo256.png'} size={'48px'} d={'inline'} />
+                    <Text d={'inline'}>LendTree - The Social Lending App</Text>
+                </span>
                 {isAuthenticated && (
                     <IconButton variant={'outline'} variantColor={'teal'} aria-label="Open Drawer" icon={'arrow-left'} onClick={onOpen} />
                 )}
             </Flex>
+
             <Drawer isOpen={isOpen} placement={'right'} onClose={onClose}>
                 <DrawerOverlay />
                 <DrawerContent>
