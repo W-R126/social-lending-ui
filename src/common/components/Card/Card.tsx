d00fabfae10d4e92c231a9273dfedac2f9fc01f0
rz-roy <rz.royzhang@gmail.com>
2020-11-13 15:30:45 +0100

    increase padding of card a bit

--- src/common/components/Card/Card.tsx
+++ src/common/components/Card/Card.tsx
@@ -3,7 +3,7 @@ import {Box, BoxProps} from '@chakra-ui/core';
 
 export const Card: React.FC<BoxProps> = ({children, ...rest}) => {
     return (
-        <Box p={4} shadow={'md'} borderWidth={'1px'} rounded={'md'} {...rest}>
+        <Box p={6} shadow={'md'} borderWidth={'1px'} rounded={'md'} {...rest}>
             {children}
         </Box>
     );
