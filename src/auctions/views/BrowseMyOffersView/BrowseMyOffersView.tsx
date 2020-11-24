420ff22c077b989c469accf7963d8065e91ff0a5
rz-roy <rz.royzhang@gmail.com>
2020-11-24 18:45:16 +0100

    add card to offer list

--- src/auctions/views/BrowseMyOffersView/BrowseMyOffersView.tsx
+++ src/auctions/views/BrowseMyOffersView/BrowseMyOffersView.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import {useLenderOffers} from '../../hooks/useLenderOffers';
 import {useInit} from '../../../common/hooks/useInit';
-import {Flex, Skeleton} from '@chakra-ui/react';
+import {Skeleton, Stack, Stat, StatLabel, StatNumber, StatHelpText, StatGroup, IconButton, Box} from '@chakra-ui/react';
+import {Card} from '../../../common/components/Card';
+import {Delete, Trash} from 'react-feather';
 
 export const BrowseMyOffersView: React.FC = () => {
     const {offers, isFetching, fetchOffers} = useLenderOffers();
@@ -9,9 +11,25 @@ export const BrowseMyOffersView: React.FC = () => {
 
     return (
         <Skeleton isLoaded={!isFetching}>
-            {offers.map(offer => (
-                <div>{offer}</div>
-            ))}
+            <Stack m={3} align={'center'}>
+                {offers.map(offer => (
+                    <Card maxWidth={'500px'} width={'full'}>
+                        <StatGroup>
+                            <Stat>
+                                <StatLabel>Amount</StatLabel>
+                                <StatNumber>${offer.auction.loanAmount.toFixed(2)}</StatNumber>
+                            </Stat>
+
+                            <Stat>
+                                <StatLabel>Interest</StatLabel>
+                                <StatNumber>{offer.annualPercentageRate.toFixed(2)}%</StatNumber>
+                            </Stat>
+
+                            <IconButton colorScheme={'red'} aria-label={'Delete offer'} icon={<Trash />} />
+                        </StatGroup>
+                    </Card>
+                ))}
+            </Stack>
         </Skeleton>
     );
 };
