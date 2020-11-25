34de7fb6653a698c1cf3dc0d858afbc388a67e66
rz-roy <rz.royzhang@gmail.com>
2020-11-25 21:45:56 +0000

    not sure why 401

--- src/userProfile/hooks/useTransactions.ts
+++ src/userProfile/hooks/useTransactions.ts
@@ -4,11 +4,11 @@ import {topUpAccount} from '../api/account/accountApi';
 
 export function useTransactions() {
     const [isFetching, setFetching] = useState(false);
-    setFetching(false); // todo temp
+    // setFetching(false); // todo temp
 
-    async function topUp(accountNo: string, amount: number): Promise<boolean> {
+    async function sendTopUp(accountNo: string, amount: number): Promise<boolean> {
         return (await topUpAccount(accountNo, amount)) !== null;
     }
 
-    return {isFetching, topUp};
+    return {isFetching, sendTopUp};
 }
