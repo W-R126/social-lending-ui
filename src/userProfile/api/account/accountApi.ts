cdb3e2f133cc82da3a5b20e93b6aefa8ea5ac5a3
rz-roy <rz.royzhang@gmail.com>
2020-11-26 21:55:54 +0000

    updated accountApi.ts

--- src/userProfile/api/account/accountApi.ts
+++ src/userProfile/api/account/accountApi.ts
@@ -4,13 +4,13 @@ import {BASE_URL} from './accountApi.constants';
 
 const instance = axios.create({
     baseURL: BASE_URL,
-    headers: {
-        Authorization: `Basic ZXhhbXBsZTpleGFtcGxl`,
+    auth: {
+        username: 'example',
+        password: 'example',
     },
-    //    auth: {
-    //        username: 'example',
-    //        password: 'example'
-    //    }
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+    },
 });
 
 export function getAccountDetails(accountNo: string): Promise<AccountDetails | null> {
@@ -22,7 +22,7 @@ export function getAccountDetails(accountNo: string): Promise<AccountDetails | n
 
 export function topUpAccount(accountNo: string, amount: number): Promise<boolean> {
     return instance
-        .post('/payments', {accountNumber: accountNo, amount: amount}, {headers: {Authorization: `Basic ZXhhbXBsZTpleGFtcGxl`}})
+        .post(BASE_URL + '/payments', {accountNumber: accountNo, amount: amount})
         .then(response => {
             console.log(response);
             return true;
