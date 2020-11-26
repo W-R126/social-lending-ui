ca90caf3e814bafc549e91303329165f812dfe7a
rz-roy <rz.royzhang@gmail.com>
2020-11-26 18:31:56 +0100

    Small fixes

--- src/auctions/components/AuctionInfo/AuctionInfo.tsx
+++ src/auctions/components/AuctionInfo/AuctionInfo.tsx
@@ -10,36 +10,34 @@ interface Props {
 
 export const AuctionInfo: React.FC<Props> = ({auction, auctionId}) => {
     return (
-        <>
-            <Skeleton isLoaded={!!auction}>
-                <Box minHeight={120} mt={5} shadow={'sm'} borderRadius={'lg'}>
-                    <Flex justifyContent={'center'} mb={5}>
-                        <Heading as={'h3'} size={'lg'}>
-                            Auction No. {auctionId ?? ''}
-                        </Heading>
+        <Skeleton isLoaded={!!auction}>
+            <Box minHeight={120} mt={5} shadow={'sm'} borderRadius={'lg'}>
+                <Flex justifyContent={'center'} mb={5}>
+                    <Heading as={'h3'} size={'lg'}>
+                        Auction No. {auctionId ?? ''}
+                    </Heading>
+                </Flex>
+                <Flex justifyContent={'space-around'}>
+                    <Flex>
+                        <Stat>
+                            <StatLabel>Number of installments</StatLabel>
+                            <StatNumber>{auction?.numberOfInstallments}</StatNumber>
+                        </Stat>
                     </Flex>
-                    <Flex justifyContent={'space-around'}>
-                        <Flex>
-                            <Stat>
-                                <StatLabel>Number of installments</StatLabel>
-                                <StatNumber>{auction?.numberOfInstallments}</StatNumber>
-                            </Stat>
-                        </Flex>
-                        <Flex>
-                            <Stat>
-                                <StatLabel>Loan amount</StatLabel>
-                                <StatNumber>
-                                    {CURRENCY}
-                                    {auction?.loanAmount}
-                                </StatNumber>
-                            </Stat>
-                        </Flex>
+                    <Flex>
+                        <Stat>
+                            <StatLabel>Loan amount</StatLabel>
+                            <StatNumber>
+                                {CURRENCY}
+                                {auction?.loanAmount}
+                            </StatNumber>
+                        </Stat>
                     </Flex>
-                    <Flex alignItems={'center'} p={8} flexDir={'column'} flexWrap={'wrap'}>
-                        <Text size={'lg'}>{auction?.description}</Text>
-                    </Flex>
-                </Box>
-            </Skeleton>
-        </>
+                </Flex>
+                <Flex alignItems={'center'} p={8} flexDir={'column'} flexWrap={'wrap'}>
+                    <Text size={'lg'}>{auction?.description}</Text>
+                </Flex>
+            </Box>
+        </Skeleton>
     );
 };
