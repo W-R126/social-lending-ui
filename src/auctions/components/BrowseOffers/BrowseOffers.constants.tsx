1bc23f51464f154c2583280aebc7beaa7963e741
rz-roy <rz.royzhang@gmail.com>
2020-11-26 18:33:54 +0100

    Add acceptation of offer

--- src/auctions/components/BrowseOffers/BrowseOffers.constants.tsx
+++ src/auctions/components/BrowseOffers/BrowseOffers.constants.tsx
@@ -5,19 +5,23 @@ import {HandleAcceptOfferClick} from './BrowseOffers.helpers';
 import {TableButton} from '../TableButton';
 import {DATE_FORMAT} from '../../../common/constants';
 
-export const TableColumns: any = [
-    {
-        Header: 'Annual percentage rate',
-        accessor: 'proposedAnnualPercentageRate',
-        Cell: (props: any) => <Box>{props.value * 100} %</Box>,
-    },
-    {
-        Header: 'Date',
-        accessor: 'date',
-        Cell: (props: any) => <Box>{format(new Date(props.value), DATE_FORMAT)}</Box>,
-    },
-    {
-        Header: 'Action',
-        Cell: (props: any) => <TableButton text="Accept" onClick={() => HandleAcceptOfferClick(props.cell.row.original)} />,
-    },
-];
+export const TableColumns: any = (auctionId: number, history: History) => {
+    return [
+        {
+            Header: 'Annual percentage rate',
+            accessor: 'proposedAnnualPercentageRate',
+            Cell: (props: any) => <Box>{props.value * 100} %</Box>,
+        },
+        {
+            Header: 'Date',
+            accessor: 'date',
+            Cell: (props: any) => <Box>{format(new Date(props.value), DATE_FORMAT)}</Box>,
+        },
+        {
+            Header: 'Action',
+            Cell: (props: any) => (
+                <TableButton text="Accept" onClick={() => HandleAcceptOfferClick(props.cell.row.original.id, auctionId, history)} />
+            ),
+        },
+    ];
+};
--- src/auctions/components/BrowseOffers/BrowseOffers.helpers.ts
+++ src/auctions/components/BrowseOffers/BrowseOffers.helpers.ts
@@ -1,5 +1,10 @@
-import {OfferDTO} from '../../api/offersAPI.types';
+import {acceptOffer} from '../../../loans/api/borrower/loansAPI';
+import {Routes} from '../../../routing/routes';
 
-export const HandleAcceptOfferClick = (offer: OfferDTO) => {
-    console.log(offer);
+export const HandleAcceptOfferClick = async (offerId: number, auctionId: number, history: any) => {
+    if ((await acceptOffer(Number(auctionId), Number(offerId))) !== null) {
+        history.push(Routes.BORROWER_LOANS);
+    } else {
+        console.log('Sth went wrong');
+    }
 };
